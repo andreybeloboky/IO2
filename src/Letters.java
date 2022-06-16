3e7e0fa97280af7057a4324381f8c76d3ca3198f
Andrey Beloboky <andrei.beloboky@yandex.ru>
2022-06-16 21:33:37 +0300

    create class Letters to find nummers of letters

---
+++ src/Letters.java
@@ -0,0 +1,79 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Letters {
+    public static int[] findLetters(BufferedReader readText) throws IOException {
+        int a = 0;
+        int e = 0;
+        int i = 0;
+        int o = 0;
+        int y = 0;
+        int u = 0;
+        String line;
+        while ((line = readText.readLine()) != null) {
+            char[] line1 = line.toCharArray();
+            for (char value : line1) {
+                if (value == 'a') {
+                    a++;
+                }
+                if (value == 'e') {
+                    e++;
+                }
+                if (value == 'i') {
+                    i++;
+                }
+                if (value == 'o') {
+                    o++;
+                }
+                if (value == 'y') {
+                    y++;
+                }
+                if (value == 'u') {
+                    u++;
+                }
+            }
+        }
+        return new int[]{a, e, i, o, y, u};
+    }
+
+    public static long[] findLettersInputStream(BufferedInputStream read) throws IOException {
+        long a = 0;
+        long e = 0;
+        long i = 0;
+        long o = 0;
+        long y = 0;
+        long u = 0;
+        long z;
+        while ((z = read.read()) != -1) {
+                if ((char) z == 'a') {
+                    a++;
+                }
+                if ((char) z == 'e') {
+                    e++;
+                }
+                if ((char) z == 'i') {
+                    i++;
+                }
+                if ((char) z == 'o') {
+                    o++;
+                }
+                if ((char) z == 'y') {
+                    y++;
+                }
+                if ((char) z == 'u') {
+                    u++;
+                }
+            }
+        return new long[]{a, e, i, o, y, u};
+    }
+
+    public static String[] returnString() {
+        String[] letters = {"a", "e", "i", "o", "y", "u"};
+        String[] fullName = new String[letters.length];
+        for (int i = 0; i<letters.length;i++) {
+          fullName[i] = "Numbers of " + letters[i] + " is ";
+        }
+        return fullName;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import java.io.*;
+
 public class Main {
+    private static Object Charsets;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        File file = new File("text.txt");
+        // big file
+        try (BufferedWriter write = new BufferedWriter(new FileWriter(file))) {
+            for (long i = 0; i <= 1000000000; i++) {
+                write.write("ahahahahahahahahahahahah");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try (BufferedInputStream read = new BufferedInputStream(new FileInputStream(file), 200)) {
+            long[] numbers = Letters.findLettersInputStream(read);
+            String[] name = Letters.returnString();
+            for (int i = 0; i < numbers.length; i++) {
+                System.out.println(name[i] + numbers[i]);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println();
+        // small file
+        File file1 = new File("textSmall.txt");
+        try (FileWriter write = new FileWriter(file1);
+             BufferedWriter write1 = new BufferedWriter(write)) {
+            write1.write("My name's Andrew. Test of textSmall doc" + "\nIt's a new line");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try (BufferedReader readText2 = new BufferedReader(new FileReader(file1))) {
+            int[] number = Letters.findLetters(readText2);
+            String[] name = Letters.returnString();
+            for (int i = 0; i < number.length; i++) {
+                System.out.println(name[i] + number[i]);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
